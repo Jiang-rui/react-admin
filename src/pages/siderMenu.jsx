7e690f63d5af39438f03306c59d9e645a3cc5497
蒋锐 <jiangrui@dztec.net>
2019-07-23 16:24:45 +0800

    配置了项目commit的eslint校验

--- src/pages/siderMenu.jsx
+++ src/pages/siderMenu.jsx
@@ -7,6 +7,38 @@ import { Menu, Icon, } from 'antd';
 import { routerLink } from '../router/config';
 const { SubMenu } = Menu;
 export default class SiderMenu extends Component {
+  // 渲染侧边栏导航栏
+  renderSideBar = () => {
+    return (routerLink.map((router, index) => {
+      let menu = '';
+      if (router.subMenu && router.subMenu.length > 0) {
+        let menuList = router.subMenu.map((subRouter, subIndex) => (
+          <Menu.Item key={subRouter.Link}>
+            <Link to={subRouter.Link}>
+              <span>{subRouter.name}</span>
+            </Link>
+          </Menu.Item>
+        ))
+        menu = <SubMenu
+          key={router.name}
+          title={
+            <span>
+              <Icon type={router.icon} />
+              <span>{router.name}</span>
+            </span>
+          }
+        >{menuList}</SubMenu>
+      } else {
+        menu = <Menu.Item key={router.Link}>
+          <Link to={router.Link}>
+            <Icon type={router.icon} />
+            <span>{router.name}</span>
+          </Link>
+        </Menu.Item>
+      }
+      return menu;
+    }))
+  }
   render() {
     return (
       <Menu
@@ -15,35 +47,7 @@ export default class SiderMenu extends Component {
         theme="dark"
       >
         {
-          routerLink.map((router, index) => {
-            let menu = '';
-            if (router.subMenu && router.subMenu.length > 0) {
-              let menuList = router.subMenu.map((subRouter, subIndex) => (
-                <Menu.Item key={subRouter.Link}>
-                  <Link to={subRouter.Link}>
-                    <span>{subRouter.name}</span>
-                  </Link>
-                </Menu.Item>
-              ))
-              menu = <SubMenu
-                key={router.name}
-                title={
-                  <span>
-                    <Icon type={router.icon} />
-                    <span>{router.name}</span>
-                  </span>
-                }
-              >{menuList}</SubMenu>
-            } else {
-              menu = <Menu.Item key={router.Link}>
-                <Link to={router.Link}>
-                  <Icon type={router.icon} />
-                  <span>{router.name}</span>
-                </Link>
-              </Menu.Item>
-            }
-            return menu;
-          })
+          this.renderSideBar()
         }
       </Menu>
     )
